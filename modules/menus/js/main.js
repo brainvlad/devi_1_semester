46431bf652f588a6eecb205dfcc09d8e2ffaafea
brainvlad <wladiik@gmail.com>
2021-11-13 12:27:18 +0300

    fixes

--- modules/menus/js/main.js
+++ modules/menus/js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    // Добавление иконок разворота меню
+    const textFirst = $("#first_step").text();
+    $("#first_step").text(textFirst + '⤵')
+    const secondStep = $("#second_step").text();
+    $("#second_step").text(secondStep + '⤵')
+
     $("#first_step").click(function () {
         if($('.sub-menu-vertical__list').css('display') == "none"){
             $(".sub-menu-vertical__list").show(300);
